31b5c1397f8716af66636ac0932aea9220372d63
goalzz85 <goalzz85@gmail.com>
2024-01-18 17:38:13 +0800

    a datax writer plugin that writes data to orc files

---
+++ src/main/java/com/alibaba/datax/plugin/writer/orcfilewriter/Key.java
@@ -0,0 +1,12 @@
+package com.alibaba.datax.plugin.writer.orcfilewriter;
+
+public class Key {
+    public static final String PATH = "path";
+    public static final String FILE_NAME = "fileName";
+
+    public static final String COLUMN = "column";
+
+    public static final String COLUMN_NAME = "name";
+    public static final String COLUMN_TYPE = "type";
+
+}
